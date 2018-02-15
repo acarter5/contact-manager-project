13e2a1f6029a0382dee24ae813878b61e9767c3a
Adam Carter <carter.c.adam@gmail.com>
2018-02-15 08:56:33 -0800

    further refactored UI operations out of constuctor prototype

--- operations.js
+++ operations.js
@@ -1,314 +1,295 @@
 
 
+
+/* helper functions*/
+
 function isEmpty( el ){
   return !$.trim(el.html())
 }
 
-function contactRendered(contact) {
-  var isRendered = false;
-  var $renderedContacts = $contactsContainer.find('input[type=hidden]');
-  $renderedContacts.each(function() {
-    if(Number($(this).val()) === +contact.id) {
-      isRendered = true;
+function contactRendered(id) {
+  var $contacts = $contactsContainer.find('input[type=hidden]'),
+      rendered = false;
+
+  $contacts.each(function(){
+    debugger;
+    var $contactIdentifier = $(this);
+    if (Number($contactIdentifier.val()) === id) {
+      rendered = true;
+      return;
     }
   });
-  return isRendered;
-}
+
+  return rendered;
+};
+
+/* Contact Constuctor*/
 
 function Contact(name, email, phone, tags, id) {
   this.name = name || '';
   this.email = email || '';
-  this.phone = phone || '';
-  this.id = id;
+  this.phone = phone || ''; 
   this.tags = tags || [];
-} 
+  this.id = id;
+}
 
-/* Contacts Object */
+/* Contact Collection Constructor*/
 
 function ContactsObject() {
-  this.contactsCollection = [];
+  this.collection = [];
   this.lastId = 0;
+  this.curContact = '';
 }
 
 ContactsObject.prototype = {
-  makeContact: function(name, email, phone, tags, id) {
-    console.log(this.lastId);
-    var contact = new Contact('', '', '', '', this.lastId);
+  makeContact: function() {
+    var contact = new Contact('', '', '', [], this.lastId);
     this.lastId++;
-    this.contactsCollection.push(contact);
-
+    this.collection.push(contact);
     return contact;
   },
-  removeFromCollection: function(id) {
+  remove: function(id) {
     var idx;
 
-    this.contactsCollection.forEach(function(obj, index) {
-      if (obj.id === id) {
+    this.collection.forEach(function(contact, index) {
+      if (contact.id === id) {
         idx = index;
-        return;
       }
     });
 
-    this.contactsCollection.splice(idx, 1);
+    this.collection.splice(idx, 1);
+  },
+  get: function(id) {
+    return this.collection.filter(function(contact) {
+      return  contact.id === id;
+    }).pop();
+  }, 
+}
+
+/* UI Methods*/ 
+var UI = {
+  displayContacts: function(contacts) {
+    $contactsContainer.html('');
+
+    contacts.forEach(function(contact) { 
+      contactObjArray = contact.tags.map(function(tagName) {
+        return {tag: tagName}
+      });
+
+      contact.tagObjs = contactObjArray;
+      $contactsContainer.append(contactScript(contact));
+    });
+  },
+  renderTagControl: function(tagName) {
+    $tagsControlContainer.find('ul').append(tagsControlScript({tag: tagName}));
+  },
+  renderForm: function(contact) {
+    $formContainer.find('input[name^=full-name]').val(contact.name);
+    $formContainer.find('input[name^=email]').val(contact.email);
+    $formContainer.find('input[name^=phone]').val(contact.phone),
+    
+    $('.form input[type=checkbox]').each(function() {
+      var $checkbox = $(this);
+      contact.tags.forEach(function(tag){
+        if($checkbox.val() === tag) {
+          $checkbox.prop('checked', true);
+        }
+      })
+    });
+  },
+  showform: function() {
+    $controlContainer.hide();
+    $contactsContainer.hide();
+    $noContactsDisplay.hide();
+    $formContainer.show();
+  },
+  formDone: function() {
+    document.getElementById('contact-form').reset();
+    $formContainer.hide();
+    $controlContainer.show();
+    $contactsContainer.show();
+
+    if (isEmpty($contactsContainer)) {
+      $noContactsDisplay.show();
+    }
   },
 }
 
-/* Events Obejct*/
+/* Manager Constructor*/
 
 function Manager() {
-  this.contactsObject = new ContactsObject;
-  this.tagCollection = [];
+  this.contacts = new ContactsObject;
   this.curContact = '';
+  this.tagsCollection = [];
   this.init();
 }
 
 Manager.prototype = {
-  filterContacts: function(event) {
-    var query = new RegExp($searchInput.val(), 'i'),
-        $controlTags = $('.tags-control input[type=checkbox]:checked'),
-        $renderedContacts = $contactsContainer.find('.contact');
-        controlTagsArray = [],
-        searchFilteredArray = [],
-        tempArray = [];
+  processNewContact: function(event) {
+    event.preventDefault();
+    var contact = this.contacts.makeContact();
+    this.curContact = contact;
+    UI.showform();
+  },
+  cancelContact: function(event) {
+    event.preventDefault();
+    if (!(contactRendered(this.curContact.id))) {
+      this.contacts.remove(this.curContact.id);
+    }
 
-    $controlTags.each(function() {
-      controlTagsArray.push($(this).val());
+    UI.formDone();
+  },
+  editContact: function(event) {
+    event.preventDefault();
+    var id = this.getId(event);
+    this.curContact = this.contacts.get(id);
+    UI.renderForm(this.curContact);
+    UI.showform();
+  },
+  getId: function(event) {
+    var $parent = $(event.target).closest('li');
+    return Number($parent.find('input[type=hidden]').val()); 
+  },
+  deleteContact: function(event) {
+    event.preventDefault();
+    var id = this.getId(event);
+    this.contacts.remove(id);
+    UI.displayContacts(this.contacts.collection);
+
+    if (isEmpty($contactsContainer)) {
+      $noContactsDisplay.show();
+    }
+  },
+  renderContact: function(event) {
+    event.preventDefault();
+    var contact = this.curContact;
+        name = $formContainer.find('input[name^=full-name]').val(),
+        email = $formContainer.find('input[name^=email]').val(),
+        phone = $formContainer.find('input[name^=phone]').val(),
+        tags = [],
+        tagDOMElements = $('.form input[type=checkbox]:checked');
+
+    tagDOMElements.each(function() {
+      tags.push($(this).val());
     });
 
-    this.contactsObject.contactsCollection.forEach(function(contact) {
+    contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+    contact.tags = tags;
+
+    UI.displayContacts(this.contacts.collection);
+    UI.formDone();
+  },
+  filterContacts: function() {
+    var query = new RegExp($searchInput.val(), 'i'),
+        searchFilteredArray = [],
+        tempArray = []
+        tags = $controlContainer.find('input[type=checkbox]:checked');
+
+    this.contacts.collection.forEach(function(contact) {
       if (contact.name.match(query)) {
         searchFilteredArray.push(contact);
       }
     });
 
-    controlTagsArray.forEach(function(controlTag) {
+    tags.each(function() {
+      var controlTag = $(this).val();
+      debugger;
       searchFilteredArray.forEach(function(contact) {
-        if (($.inArray(String(controlTag), contact.tags)) !== -1) {
+        if ($.inArray(controlTag, contact.tags) !== -1) {
           tempArray.push(contact);
         }
       });
       searchFilteredArray = tempArray;
       tempArray = [];
     });
-
-    $renderedContacts.hide();
-
-    searchFilteredArray.forEach(function(contact) {
-      $renderedContacts.each(function() {
-        var $curRenderedContact = $(this);
-
-        if (Number(contact.id) === Number($curRenderedContact.find('input[type=hidden]').val())) {
-          $curRenderedContact.show();
-        }
-
-      });
-    }.bind(this));
+    UI.displayContacts(searchFilteredArray);
   },
-  deleteContact: function(event) {
+  processNewTag: function(event) {
     event.preventDefault();
-    var id = Number(this.findId(event)),
-        $parent = this.findRenderedContact(id),
-        idx;
-
-    this.contactsObject.removeFromCollection(id);
-
-    $parent.remove();
-
-    if (isEmpty($('#contacts-container'))) {
-      $('.no-contacts').show();
+    var tagName;
+    if ($tagInput.val() !== '') {
+      tagName = $tagInput.val();
+      UI.renderTagControl(tagName);
+      $tagInput.val('');
     }
-  },
-  editContact: function(event) {
-    event.preventDefault();
-    var id = Number(this.findId(event));
-
-    this.curContact = this.contactsObject.contactsCollection.filter(function(obj) {
-       return obj.id === id;
-    }).pop();
-
-    this.showForm();
-  },
-  cancelContact(event) {
-    event.preventDefault();
-
-    if(!(contactRendered(this.curContact))) {
-      this.contactsObject.removeFromCollection(this.curContact.id);
-    }
-
-    this.formDone();
-  },
-  findRenderedContact: function(id) {
-    var $input = $('input[type=hidden]').filter(function() {
-      return Number($(this).val()) === id;
-    });
-
-    return $input.closest('li');
-  },
-  findId: function(event) {
-    var $parent = $(event.target).closest('li');
-    return $parent.find('input[type=hidden]').val();
-  },
-  displayContact: function(contact) {
-    var contactHTML = contactScript(contact);
-
-    if (contactRendered(contact)) {
-      $parent = this.findRenderedContact(contact.id);
-      $parent.html(partialContactScript(contact));
-    } else {
-      $contactsContainer.append(contactHTML);
-    }
-  },
-  renderContact: function() {
-      var contact = this.curContact,
-          $parent;
-
-      contact.name = $('input[name^=full-name]').val();
-      contact.email = $('input[name^=email]').val();
-      contact.phone = $('input[name^=phone]').val();
-
-      $('#contact-form').find('input[type=checkbox]:checked').each(function() {
-        contact.tags.push($(this).val());
-      });
-
-      contact.tagObjs = contact.tags.map(function(newTag) {
-        return {tag: newTag};
-      });
-
-      this.displayContact(contact);
-      
-      this.formDone();
-    },
-  newContact: function(event) {
-      event.preventDefault(); 
-      this.renderContact();
-    },
-  showForm: function() {
-      var contact = this.curContact;       
-
-      $well.hide();
-      $noContacts.hide();
-      $contactsContainer.hide();
-      $contactForm.show();
-
-      $('input[name^=full-name]').val(contact.name); 
-      $('input[name^=email]').val(contact.email);
-      $('input[name^=phone]').val(contact.phone);
-    },
-    formDone() {
-      document.getElementById('contact-form').reset();
-      $contactForm.hide();
-      $contactsContainer.show();
-      $well.show();
-      if (isEmpty($('#contacts-container'))) {
-        $('.no-contacts').show();
-      }
-    },
-  processContact: function(event) {
-      event.preventDefault()
-      var contact = this.contactsObject.makeContact();
-      this.curContact = contact;
-      this.showForm();
-    },
-  renderTags: function() {
-      var tagsObjectArray = this.tagCollection.map(function(newTag) {
-        return {tag: newTag};
-      }.bind(this));
-
-      $('.tags dd').html('');
-      $('.tags dd').append(tagScript({tags: tagsObjectArray}));
-    },
-  newTag: function(event) {
-      event.preventDefault();
-      var tagsArray = this.tagCollection;
-
-      if($tagInput.val() !== '') {
-        tagsArray.push($tagInput.val());
-        $tagInput.val('');
-      }
-
-      this.renderTags();
-    },
-  registerTemplates: function() {
-    tagTemplate = $('#tags').html(),
-    tagScript = Handlebars.compile(tagTemplate),
-    tagsToRenderTemplate = $('#tagsToRender').html();
-
-    Handlebars.registerPartial('tagsPartial', tagsToRenderTemplate);
-
-    contactContent = $('#contactContent').html();
-    partialContactScript = Handlebars.compile(contactContent);
-
-    Handlebars.registerPartial('contactContentPartial', contactContent);
-
-    contactTemplate = $('#contact').html(),
-    contactScript = Handlebars.compile(contactTemplate);
-  },
-  registerDOMElements: function() {
-    $contactsContainer = $('#contacts-container'),
-    $contactForm = $('.form'),
-    $noContacts = $('.no-contacts'),
-    $well = $('.well'),
-    $contactBtn = $('.add-contact'),
-    $submitBtn = $('.submit-button'),
-    $cancelBtn = $('#cancel'),
-    $tagBtn = $('.add-tag'),
-    $tagInput = $('.tag-input'),
-    $searchInput = $('.contact-name-search');
-  },
-  bindEvents: function() {
-    $contactBtn.on('click', this.processContact.bind(this));
-    $submitBtn.on('click', this.newContact.bind(this));
-    $cancelBtn.on('click', this.cancelContact.bind(this));
-    $contactsContainer.on('click', '.edit-contact', this.editContact.bind(this));
-    $contactsContainer.on('click', '.delete-contact', this.deleteContact.bind(this));
-    $tagBtn.on('click', this.newTag.bind(this));
-    $searchInput.on('input', this.filterContacts.bind(this));
-    $('.tags-control').on('change', 'input[type=checkbox]', this.filterContacts.bind(this));
-    $(window).on('unload', this.storeData.bind(this));
+    this.tagsCollection.push(tagName);
   },
   init: function() {
     this.registerDOMElements();
-    this.registerTemplates();
     this.bindEvents();
-    this.renderStoredData();
+    this.registerTemplates();
 
-    if (isEmpty($('#contacts-container'))) {
-        $('.no-contacts').show();
-      }
-  },
-  storeData: function(event) {
+    this.retrieveData();
 
-    if ($('.form:visible')) {
-      this.cancelContact();
-    }
-    var collecionString = JSON.stringify(this.contactsObject.contactsCollection),
-        tagsString = JSON.stringify(this.tagCollection);
-
-    localStorage.setItem('lastId', this.contactsObject.lastId);
-    localStorage.setItem('collectedContacts', collecionString);
-    localStorage.setItem('collectedTags', tagsString);
-  },
-  renderStoredData: function() {
-    var lastId = localStorage.getItem('lastId'),
-        collection = JSON.parse(localStorage.getItem('collectedContacts')),
-        tagCollection = JSON.parse(localStorage.getItem('collectedTags'));
-
-    if (lastId) {
-      this.contactsObject.lastId = lastId;
-      this.contactsObject.contactsCollection = collection;
-
-      this.contactsObject.contactsCollection.forEach(function(contact) {
-        this.displayContact(contact);
-      }.bind(this));
-
-      if (tagCollection) {
-        this.tagCollection = tagCollection;
-        this.renderTags();
-      }
+    if (isEmpty($contactsContainer)) {
+      $noContactsDisplay.show();
     }
   },
+  registerDOMElements: function() {
+    $controlContainer = $('.well');
+    $formContainer = $('.form');
+    $tagsControlContainer = $('.tags');
+    $contactsContainer = $('#contacts-container');
+    $noContactsDisplay = $('.no-contacts');
+    $addContactBtn = $('.add-contact');
+    $addTagBtn = $('.add-tag');
+    $tagInput = $('.tag-input');
+    $searchInput = $('.contact-name-search');
+    $submitContactBtn = $('.submit-button');
+    $cancelContactBtn = $('.cancel-button');
+  },
+  registerTemplates: function() {
+    Handlebars.registerPartial('tagsPartial', $('#tags-to-render').html());
+    contactContentScript = Handlebars.compile($('#contact-content').html());
+    Handlebars.registerPartial('contactContentPartial', $('#contact-content').html());
+    contactScript = Handlebars.compile($('#contact').html());
+
+    tagsControlScript = Handlebars.compile($('#tags-control-template').html());
+  },
+  bindEvents: function() {
+    $addContactBtn.on('click', this.processNewContact.bind(this));
+    $addTagBtn.on('click', this.processNewTag.bind(this));
+    $submitContactBtn.on('click', this.renderContact.bind(this));
+    $cancelContactBtn.on('click', this.cancelContact.bind(this));
+    $contactsContainer.on('click', '.edit-contact', this.editContact.bind(this));
+    $contactsContainer.on('click', '.delete-contact', this.deleteContact.bind(this));
+    $searchInput.on('input', this.filterContacts.bind(this));
+    $controlContainer.on('change', 'input[type=checkbox]', this.filterContacts.bind(this));
+    $(window).on('unload', this.storeData.bind(this));
+  },
+  storeData: function() {
+    var collection = JSON.stringify(this.contacts.collection),
+        tags = JSON.stringify(this.tagsCollection);
+
+    localStorage.setItem('collection', collection);
+    localStorage.setItem('lastId', this.contacts.lastId);
+    localStorage.setItem('tags', tags);
+  },
+  retrieveData: function() {
+    var collection = JSON.parse(localStorage.getItem('collection')),
+        lastId = localStorage.getItem('lastId'),
+        tags = JSON.parse(localStorage.getItem('tags'));
+
+    if(lastId) {
+      this.contacts.lastId = lastId;
+      this.contacts.collection = collection;
+    }
+
+    if (this.contacts.collection.length > 0) {
+      UI.displayContacts(this.contacts.collection);
+    }
+
+    if (tags) {
+      tags.forEach(function(tagName) {
+       UI.renderTagControl(tagName);
+      });
+    }
+  }
 }
 
 $(function() {
-  new Manager;
+  new Manager();
 });
+
